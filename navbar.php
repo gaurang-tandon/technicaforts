714f5aede319277f5d30ad3b00939ed7c19c8353
outpost <outpostclient@gmail.com>
2024-12-19 12:03:15 +0530

    add changes in index.php

---
+++ navbar.php
@@ -0,0 +1,92 @@
+<nav class="navbar navbar-expand-lg bg-white shadow px-2 px-lg-5">
+        <div class="container-fluid">
+            <!-- Logo -->
+            <a class="navbar-brand d-flex align-items-center py-0" href="index.php">
+                <img src="./images/index/logo.png" alt="Logo" class="logo-img" />
+            </a>
+
+            <!-- Navbar Toggler (for Mobile View) -->
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="container align-items-center justify-content-between">
+                <!-- Center Branding -->
+                <!-- <div class="col-4">
+          <h2 id="company_name" class="fw-bold mb-0 iceland-regular">Technica Forts</h2>
+        </div> -->
+
+                <!-- Links (Collapsed in Mobile) -->
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav mx-auto">
+                        <li class="nav-item mx-lg-3 rounded-3">
+                            <a class="nav-link" href="index.php">Home</a>
+                        </li>
+                        <li class="nav-item mx-lg-3 rounded-3 dropdown">
+                            <a class="nav-link dropdown-toggle" href="./services.php" role="button" aria-expanded="false">
+                                Services
+                            </a>
+                            <ul class="dropdown-menu rounded-4 mx-auto py-3 px-2">
+                                <li>
+                                    <a class="dropdown-item rounded-5 my-1" href="./services/medical_coding.php">Medical
+                                        Coding</a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item rounded-5 my-1"
+                                        href="./services/medical_record_review.php">Medical Record Review</a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item rounded-5 my-1"
+                                        href="./services/medical_scribing.php">Medical Scribing</a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item rounded-5 my-1"
+                                        href="./services/medical_transcription.php">Medical Transcription</a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item rounded-5 my-1"
+                                        href="./services/pharmacovigilance.php">Pharmacovigilance</a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item rounded-5 my-1"
+                                        href="./services/revenue_cycle_management.php">Revenue Cycle Management</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="nav-item mx-lg-3 rounded-3">
+                            <a class="nav-link" href="./case_study.php">Case Study</a>
+                        </li>
+                        <li class="nav-item mx-lg-3 rounded-3">
+                            <a class="nav-link" href="./about_us.php">About Us</a>
+                        </li>
+                        <li class="nav-item mx-lg-3 rounded-3">
+                            <a class="nav-link" href="./resources.php">Resources</a>
+                        </li>
+                        <li class="nav-item mx-lg-3 rounded-3">
+                            <a class="nav-link" href="./contact_us.php">Contact Us</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <!-- Social Media Icons -->
+            <div class="fs-5 social-media-parent">
+                <a href="#" class="social-media rounded-3 text-decoration-none g-plus" target="_blank">
+                    <i class="fab fa-google-plus-square"></i>
+                </a>
+                <a href="#" class="social-media rounded-3 text-decoration-none facebook" target="_blank">
+                    <i class="fab fa-facebook-square"></i>
+                </a>
+                <a href="#" class="social-media rounded-3 text-decoration-none linkedin" target="_blank">
+                    <i class="fab fa-linkedin"></i>
+                </a>
+                <a href="#" class="social-media rounded-3 text-decoration-none instagram" target="_blank">
+                    <i class="fab fa-instagram"></i>
+                </a>
+                <a href="#" class="social-media rounded-3 text-decoration-none twitter" target="_blank">
+                    <i class="fab fa-twitter"></i>
+                </a>
+            </div>
+        </div>
+    </nav>
