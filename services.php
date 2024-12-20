a4bff7ccc1c5b7ba1f051072b97c01b0510098e0
outpost <outpostclient@gmail.com>
2024-12-20 17:07:02 +0530

    add changes in other files

---
+++ services.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <title>Home</title>
+
+    <!-- Bootstrap 5.3 CSS CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        crossorigin="anonymous" />
+
+    <!-- Font Awesome CDN for Icons and Fonts -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Audiowide&family=Iceland&display=swap" rel="stylesheet" />
+
+    <!-- Bootstrap 5.3 JS CDN -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
+    </script>
+
+    <link rel="stylesheet" href="./styles/services_styles.css" />
+    <link rel="stylesheet" href="./styles/index_styles.css" />
+</head>
+
+<body>
+    <!-- Navbar -->
+    <?php include "navbar.php" ?>
+
+    <div class="services-main-banner">
+        <div class="container">
+            <div class="row align-items-center justify-content-start" style="min-height: 90vh;">
+                <div class="col-12 col-lg-6">
+                    <h1 class="fw-bold fs-1">Medical Transcription</h1>
+                    <p class="fs-4">MedVoice provides state-of-the-art Medical Transcription Services that combines advanced AI Technology with expert Medical Language Specialists.</p>
+                    <button class="btn btn-primary">Let's Talk</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
