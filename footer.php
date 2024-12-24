6e46fe70cae16863c1494feb54122c72c1b384cd
gaurang-tandon <gaurang.tandon@gmail.com>
2024-12-24 11:46:52 +0530

    footer added, navbar css updated

--- footer.php
+++ footer.php
@@ -1,60 +1,99 @@
-<div class="container-fuild bg-custom-blue text-light">
-  <div class="container"><footer class="py-5">
-    <div class="row">
-      <div class="col-6 col-md-2 mb-3">
-        <h5>Section</h5>
-        <ul class="nav flex-column text-light">
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About</a></li>
-        </ul>
-      </div>
+<head>
+  <link rel="stylesheet" href="./styles/footer.css" />
+</head>
+<div class="container-fuild bg-custom-blue text-light py-0">
+  <div class="container py-0">
+    <footer class="pt-5">
+      <div class="row">
+        <div class="col-6 col-md-2 mb-3">
+          <ul class="nav flex-column footer-list">
+            <li class="nav-item mb-2  text-light"><a href="#" class="nav-link p-0 text-light">
+                <h6>Home</h6>
+              </a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 ">
+                <h6>Case Study</h6>
+              </a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 ">
+                <h6>About Us</h6>
+              </a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 ">
+                <h6>Resources</h6>
+              </a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 ">
+                <h6>Contact Us</h6>
+              </a></li>
+          </ul>
+        </div>
 
-      <div class="col-6 col-md-2 mb-3">
-        <h5>Section</h5>
-        <ul class="nav flex-column text-light">
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About</a></li>
-        </ul>
-      </div>
+        <div class="col-6 col-md-2 mb-3">
+          <h6>Services</h6>
+          <ul class="nav flex-column footer-list">
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Medical Coding</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 ">Medical Record Review</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 ">Medical Scribing</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 ">Medical Transcription</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 ">Pharmacovigilance</a></li>
+            <li class="nav-item mb-2 text-nowrap"><a href="#" class="nav-link p-0 ">Revenue Cycle Management</a></li>
+          </ul>
+        </div>
 
-      <div class="col-6 col-md-2 mb-3">
-        <h5>Section</h5>
-        <ul class="nav flex-column text-light">
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About</a></li>
-        </ul>
-      </div>
-
-      <div class="col-md-5 offset-md-1 mb-3">
-        <form>
-          <h5>Subscribe to our newsletter</h5>
-          <p>Monthly digest of what's new and exciting from us.</p>
-          <div class="d-flex flex-column flex-sm-row w-100 gap-2 text-light">
-            <label for="newsletter1" class="visually-hidden">Email address</label>
-            <input id="newsletter1" type="text" class="form-control" placeholder="Email address">
-            <button class="btn btn-primary" type="button">Subscribe</button>
+        <div class="col-6 col-md-4 mb-3">
+          <div class="row">
+            <h6 class="col-12">Address</h6>
           </div>
-        </form>
-      </div>
-    </div>
+          <div class="row ">
+            <p class="fs-6 col-5 border-end">
+              26 Grey Peeble Court,<br>
+              Darnestown MD 20874, USA
+            </p>
+            <p class="fs-6 col-7">
+              Tel: +91-933-611-3222<br>
+              Mail to: info@technicaforts.com<br>
+            </p>
+          </div>
+          <div class="row">
+            <div class="col-12">
+            Medical Transcription Training Ph: +91-75-0512-0512
+            </div>
+          </div>
+        </div>
 
-    <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
-      <p>© 2024 Company, Inc. All rights reserved.</p>
-      <ul class="list-unstyled d-flex text-light">
-        <li class="ms-3"><a class="link-body-emphasis" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"></use></svg></a></li>
-        <li class="ms-3"><a class="link-body-emphasis" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"></use></svg></a></li>
-        <li class="ms-3"><a class="link-body-emphasis" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"></use></svg></a></li>
-      </ul>
-    </div>
-  </footer>
+        <div class="col-md-3 offset-md-1 mb-3">
+          <h6>Follow Us</h6>
+          <div class="fs-5 footer-social-media-parent d-none d-lg-block">
+            <a href="#" class="social-media bg-light rounded-3 text-decoration-none g-plus" target="_blank">
+              <i class="fab fa-google-plus-square"></i>
+            </a>
+            <a href="#" class="social-media bg-light rounded-3 text-decoration-none facebook" target="_blank">
+              <i class="fab fa-facebook-square"></i>
+            </a>
+            <a href="#" class="social-media bg-light rounded-3 text-decoration-none linkedin" target="_blank">
+              <i class="fab fa-linkedin"></i>
+            </a>
+            <a href="#" class="social-media bg-light rounded-3 text-decoration-none instagram" target="_blank">
+              <i class="fab fa-instagram"></i>
+            </a>
+            <a href="#" class="social-media bg-light rounded-3 text-decoration-none twitter" target="_blank">
+              <i class="fab fa-twitter"></i>
+            </a>
+            <p>© 2024 Privacy policy</p>
+          </div>
+        </div>
+
+        <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
+          <p>© 2024 Company, Inc. All rights reserved.</p>
+          <ul class="list-unstyled d-flex text-light">
+            <li class="ms-3"><a class="link-body-emphasis" href="#"><svg class="bi" width="24" height="24">
+                  <use xlink:href="#twitter"></use>
+                </svg></a></li>
+            <li class="ms-3"><a class="link-body-emphasis" href="#"><svg class="bi" width="24" height="24">
+                  <use xlink:href="#instagram"></use>
+                </svg></a></li>
+            <li class="ms-3"><a class="link-body-emphasis" href="#"><svg class="bi" width="24" height="24">
+                  <use xlink:href="#facebook"></use>
+                </svg></a></li>
+          </ul>
+        </div>
+    </footer>
   </div>
 </div>
