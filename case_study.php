45cc223f5671bd99f5a749cd8af1cab25f623071
gaurang-tandon <gaurang.tandon@gmail.com>
2024-12-24 12:04:24 +0530

    page titles renamed, footer included in all

---
+++ case_study.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <title>Case Study</title>
+
+    <!-- Bootstrap 5.3 CSS CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        crossorigin="anonymous" />
+
+    <!-- Font Awesome CDN for Icons and Fonts -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Audiowide&family=Iceland&display=swap" rel="stylesheet" />
+
+    <!-- Bootstrap 5.3 JS CDN -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
+    </script>
+
+    <link rel="stylesheet" href="./styles/index_styles.css" />
+</head>
+
+<body>
+    <!-- Navbar -->
+    <?php include "navbar.php" ?>
+
+    <?php include "./footer.php" ?>
+</body>
+
+</html>
